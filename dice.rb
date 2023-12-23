c759da0704eb6bcbae6cf55bf4a807ae1bf8465d
Eduardo Villa <151227243+evilla092@users.noreply.github.com>
2023-12-23 19:01:02 +0000

    finished dice roll

---
+++ dice.rb
@@ -0,0 +1,70 @@
+require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+get("/") do
+  erb(:home)
+end
+
+get("/") do
+  "Hello World"
+end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server"
+end
+
+get("/dice/2/6") do
+  @rolls = []
+  2.times do
+    @rolls.push(rand(1..6))
+  end
+
+    erb(:two_six)
+end
+
+get("/dice/2/10") do
+  @rolls = []
+  2.times do
+    @rolls.push(rand(1..10))
+  end
+
+  erb(:two_ten)
+end
+
+get("/dice/1/20") do
+  @rolls = []
+  1.times do
+    @rolls.push(rand(1..20))
+  end
+
+  erb(:one_twenty)
+end
+
+get("/dice/5/4") do
+  @rolls = []
+  5.times do
+    @rolls.push(rand(1..4))
+  end
+
+
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+  100.times do
+    @rolls.push(rand(1..6))
+  end
+  erb(:one_hundred_six)
+end
